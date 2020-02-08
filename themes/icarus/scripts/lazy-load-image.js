29aea96e74af448fd7ee303bdf9fab8a43a4647a
oalieno <jeffrey6910@gmail.com>
2020-02-08 15:17:38 +0800

    fix lazy loading image

--- themes/icarus/scripts/lazy-load-image.js
+++ themes/icarus/scripts/lazy-load-image.js
@@ -1,3 +1,3 @@
 hexo.extend.filter.register('after_render:html', function (htmlContent) {
-    return htmlContent.replace(/<img src="([^"]*)" class="([^"]*)" ([^>]*)>/, '<img data-src="$1" class="$2 lazy" $3>')
+    return htmlContent.replace(/<img src="([^"]*)" (?:class="([^"]*)")?([^>]*)>/, '<img data-src="$1" class="$2 lazyload" $3>')
 });
--- themes/icarus/source/js/main.js
+++ themes/icarus/source/js/main.js
@@ -234,13 +234,13 @@
         var lazyloadImages;    
         
         if ("IntersectionObserver" in window) {
-            lazyloadImages = document.querySelectorAll(".lazy");
+            lazyloadImages = document.querySelectorAll(".lazyload");
             var imageObserver = new IntersectionObserver(function(entries, observer) {
                 entries.forEach(function(entry) {
                     if (entry.isIntersecting) {
                         var image = entry.target;
                         image.src = image.dataset.src;
-                        image.classList.remove("lazy");
+                        image.classList.remove("lazyload");
                         imageObserver.unobserve(image);
                     }
                 });
@@ -251,7 +251,7 @@
             })
         } else {  
             var lazyloadThrottleTimeout;
-            lazyloadImages = document.querySelectorAll(".lazy");
+            lazyloadImages = document.querySelectorAll(".lazyload");
             
             function lazyload () {
                 if(lazyloadThrottleTimeout) {
@@ -263,7 +263,7 @@
                     lazyloadImages.forEach(function(img) {
                         if(img.offsetTop < (window.innerHeight + scrollTop)) {
                         img.src = img.dataset.src;
-                        img.classList.remove('lazy');
+                        img.classList.remove('lazyload');
                         }
                     });
                     if(lazyloadImages.length == 0) { 
