55041b42d0eebe93dcbd2b118752f3e813f7a6ca
oalieno <jeffrey6910@gmail.com>
2020-02-08 20:14:49 +0800

    use lazysizes

--- themes/icarus/source/js/animation.js
+++ themes/icarus/source/js/animation.js
@@ -4,24 +4,24 @@
     }
 
     $('body > .navbar, body > .section, body > .footer').forEach(function (element) {
-        element.style.transition = '0s';
-        element.style.opacity = '0';
+        //element.style.transition = '0s';
+        element.style.opacity = '1';
     });
     document.querySelector('body > .navbar').style.transform = 'translateY(-100px)';
     ['.column-main > .card',
      '.column-left > .card, .column-right-shadow > .card',
      '.column-right > .card'].map(function (selector) {
         $(selector).forEach(function (element) {
-            element.style.transition = '0s';
-            element.style.opacity = '0';
-            element.style.transform = 'scale(0.8)';
-            element.style.transformOrigin = 'center top';
+            //element.style.transition = '0s';
+            element.style.opacity = '1';
+            //element.style.transform = 'scale(0.8)';
+            //element.style.transformOrigin = 'center top';
         });
     });
     setTimeout(function () {
         $('body > .navbar, body > .section, body > .footer').forEach(function (element) {
             element.style.opacity = '1';
-            element.style.transition = 'opacity 0.3s ease-out, transform 0.3s ease-out';
+            //element.style.transition = 'opacity 0.3s ease-out, transform 0.3s ease-out';
         });
         document.querySelector('body > .navbar').style.transform = 'translateY(0)';
         ['.column-main > .card',
@@ -31,8 +31,8 @@
             $(selector).forEach(function (element) {
                 setTimeout(function () {
                     element.style.opacity = '1';
-                    element.style.transform = '';
-                    element.style.transition = 'opacity 0.3s ease-out, transform 0.3s ease-out';
+                    //element.style.transform = '';
+                    //element.style.transition = 'opacity 0.3s ease-out, transform 0.3s ease-out';
                 }, i * 100);
                 i++;
             });
